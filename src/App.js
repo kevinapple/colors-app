705668f04e38db0c8472a1d7cd22b3458ccac9ca
kevinwit <kevin.zou@honeywell.com>
2019-08-24 19:28:16 +0800

    Display colors in SingleColorPalette

--- src/App.js
+++ src/App.js
@@ -16,6 +16,18 @@ class App extends Component {
     return (
       <Switch>
         <Route 
+          exact
+          path='/palette/:paletteId/:colorId'
+          render={routeProps=>(
+            <SingleColorPalette 
+              colorId={routeProps.match.params.colorId}
+              palette={generatePalette(
+              this.findPalette(routeProps.match.params.paletteId)
+              )} 
+            />
+          )}
+        />
+        <Route 
           exact 
           path='/' 
           render={(routeProps)=> (
@@ -31,11 +43,6 @@ class App extends Component {
               )} 
             />
           )}
-        />
-        <Route 
-          exact
-          path='/palette/:paletteId/:colorId'
-          render= {() => <SingleColorPalette />}
         />
       </Switch>
     );
