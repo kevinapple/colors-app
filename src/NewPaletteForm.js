2969a712f5844d4d3a828e20e37d827479d441d7
kevinwit <kevin.zou@honeywell.com>
2019-09-10 15:15:18 +0800

    refactor nav to own component

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
 import classNames from "classnames";
 import { withStyles } from "@material-ui/core/styles";
+import PaletteFormNav from "./PaletteFormNav";
 import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import clsx from 'clsx';
@@ -90,7 +86,6 @@ class NewPaletteForm extends Component {
       currentColor:"teal",
       newColorName:"",
       colors: this.props.palettes[0].colors,
-      newPaletteName:""
     };
     this.updateCurrentColor = this.updateCurrentColor.bind(this);
     this.addNewColor = this.addNewColor.bind(this);
@@ -155,11 +150,10 @@ class NewPaletteForm extends Component {
   clearColors(evt){
     this.setState({colors:[]});
   }
-  handleSubmit(){
-      let newName = this.state.newPaletteName;
+  handleSubmit(newPaletteName){
       const newPalette = {
-        paletteName:newName,
-        id:newName.toLowerCase().replace(/ /g,"-"),
+        paletteName:newPaletteName,
+        id:newPaletteName.toLowerCase().replace(/ /g,"-"),
         colors:this.state.colors
     };
     this.props.savePalette(newPalette);
@@ -178,53 +172,19 @@ class NewPaletteForm extends Component {
   };
 
   render(){
-      const {classes, maxColors} = this.props;
+      const {classes, maxColors,palettes} = this.props;
       const {open, colors} = this.state;
       const paletteIsFull = colors.length >=maxColors;
 
       return (
         <div className={classes.root}>
-          <CssBaseline />
-          <AppBar
-            position="fixed"
-            color="default"
-            className={clsx(classes.appBar, {
-              [classes.appBarShift]: open,
-            })}
-          >
-            <Toolbar>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={this.handleDrawerOpen}
-                edge="start"
-                className={clsx(classes.menuButton, open && classes.hide)}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" noWrap>
-                Persistent drawer
-              </Typography>
-              <ValidatorForm onSubmit={this.handleSubmit}>
-                <TextValidator 
-                  label="Palette Name"
-                  value={this.state.newPaletteName}
-                  name="newPaletteName"
-                  onChange={this.handleChange}
-                  validators={["required","isPaletteNameUnique"]}
-                  errorMessages={["Enter Palette Name","Name already used"]}
-                />
-                <Button 
-                  variant="contained" 
-                  color="primary"
-                  type='submit'
-                >
-                  Save Palette
-                </Button>
-              </ValidatorForm>
-              
-            </Toolbar>
-          </AppBar>
+        <PaletteFormNav 
+          open={open} 
+          classes={classes} 
+          palettes={palettes}
+          handleSubmit={this.handleSubmit}
+          handleDrawerOpen={this.handleDrawerOpen}
+        />
           <Drawer
             className={classes.drawer}
             variant="persistent"
@@ -283,7 +243,10 @@ class NewPaletteForm extends Component {
                 type='submit'
                 color="primary" 
                 disabled={paletteIsFull}
-                style={{backgroundColor:this.state.currentColor}}
+                style={{
+                  backgroundColor:paletteIsFull
+                  ? "grey"
+                  : this.state.currentColor}}
               >
                 {paletteIsFull ? "Palette Full" : "Add Color"}
               </Button>
