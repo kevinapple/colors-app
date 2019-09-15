f1b125357182a214b77ac82b2cc2aed283de0b6e
kevinwit <kevin.zou@honeywell.com>
2019-09-16 00:07:30 +0800

    rename

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -48,7 +48,7 @@ class PaletteList extends Component {
       <div className={classes.root}>
         <div className={classes.container}>
           <nav className={classes.nav}>
-            <h1 className={classes.heading}>Kevin's Colors</h1>
+            <h1 className={classes.heading}>Kevin Colors</h1>
             <Link to='/palette/new'>Create Palette</Link>
           </nav>
           <TransitionGroup className={classes.palettes}>
